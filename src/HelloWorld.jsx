ce92fea750483e80210d75ce5d3d3ccda64d286f
Heber Acosta <heber.acosta@globant.com>
2016-11-21 15:35:49 -0300

    all

---
+++ src/HelloWorld.jsx
@@ -0,0 +1,18 @@
+import React,{ Component } from 'react';
+
+export class Principal extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+        	"pokemons":[],
+        };
+    }
+
+    render(){
+      	return(
+      	   <div>
+            <h1></h1>
+           </div>
+      	);
+    }
+}
